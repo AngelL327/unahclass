a15dcf2a9359e80025e900f696b0edb47033db19
bmolina1 <bmmolina@unah.hn>
2025-04-21 11:29:01 -0600

    Optativa arte

---
+++ unahclass/lib/optativas_de_opt_page/optativas_arte_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class OptativasArteScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> clasesArte = [
+    {"nombre": "Iniciación a la Danza", "icono": Icon(Icons.group, color: Color(0XFF101c5c))},
+    {"nombre": "Danza Folklórica", "icono": FaIcon(FontAwesomeIcons.theaterMasks, color: Color(0XFF101c5c))}, 
+    {"nombre": "Teatro en Honduras", "icono": FaIcon(FontAwesomeIcons.masksTheater, color: Color(0XFF101c5c))}, 
+    {"nombre": "Fotografía", "icono": FaIcon(FontAwesomeIcons.cameraRetro, color: Color(0XFF101c5c))},
+    {"nombre": "Redacción", "icono": FaIcon(FontAwesomeIcons.pen, color: Color(0XFF101c5c))}, 
+    {"nombre": "Dibujo y Pintura", "icono": FaIcon(FontAwesomeIcons.paintBrush, color: Color(0XFF101c5c))}, 
+    {"nombre": "Apreciación Musical", "icono": FaIcon(FontAwesomeIcons.music, color: Color(0XFF101c5c))},
+    {"nombre": "Guitarra Popular", "icono": FaIcon(FontAwesomeIcons.guitar, color: Color(0XFF101c5c))}, 
+    {"nombre": "Flauta Dulce", "icono": FaIcon(FontAwesomeIcons.wind, color: Color(0XFF101c5c))}, 
+    {"nombre": "Coro Universitario", "icono": Icon(Icons.chat, color: Color(0XFF101c5c))}, 
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0XFFffffff),
+      appBar: AppBar(
+        title: Text(
+          "Arte", style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0XFF1D9FCB),
+        iconTheme: IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.help_rounded, color: Colors.white),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: Text(
+                      "Estas son las clases del área de Arte disponibles.\n¡Elige la que mejor se adapte a ti!",
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                    backgroundColor: Color(0xFFF0F0F0),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(); // Cierra el diálogo
+                        },
+                        child: Text(
+                          "CERRAR",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
