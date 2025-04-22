84f72aeb5817db583e18364af159f3284e94b6e4
bmolina1 <bmmolina@unah.hn>
2025-04-21 21:42:40 -0600

    Clases aprobadas

--- unahclass/lib/pages/clases_aprobadas_page.dart
+++ unahclass/lib/pages/clases_aprobadas_page.dart
@@ -1,236 +1,173 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'dart:convert';
+import 'package:percent_indicator/percent_indicator.dart'; // Importar el paquete
+import '../data/carreras_data.dart';
+import 'package:proyecto_prueba/widgets/helper.dart';
+class ClasesAprobadasScreen extends StatefulWidget {
+  const ClasesAprobadasScreen({super.key});
 
-class Evento {
-  final String titulo;
-  final String tipo;
-
-  Evento(this.titulo, this.tipo);
-
-  Map<String, dynamic> toJson() => {'titulo': titulo, 'tipo': tipo};
-
-  factory Evento.fromJson(Map<String, dynamic> json) {
-    return Evento(json['titulo'], json['tipo']);
-  }
-}
-
-class CalendarPage extends StatefulWidget {
   @override
-  _CalendarPageState createState() => _CalendarPageState();
+  State<ClasesAprobadasScreen> createState() => _ClasesAprobadasScreenState();
 }
 
-class _CalendarPageState extends State<CalendarPage> {
-  CalendarFormat _calendarFormat = CalendarFormat.month;
-  DateTime _focusedDay = DateTime.now();
-  DateTime? _selectedDay;
-  final Map<DateTime, List<Evento>> _eventos = {};
-
-  List<Evento> _getEventosParaDia(DateTime dia) {
-    final normalizedDate = DateTime(dia.year, dia.month, dia.day);
-    return _eventos[normalizedDate] ?? [];
-  }
-
-  Future<void> _guardarEventos() async {
-    final prefs = await SharedPreferences.getInstance();
-    final data = _eventos.map(
-      (key, value) => MapEntry(
-        key.toIso8601String(),
-        value.map((e) => e.toJson()).toList(),
-      ),
-    );
-    await prefs.setString('eventos', json.encode(data));
-  }
-
-  Future<void> _cargarEventos() async {
-    final prefs = await SharedPreferences.getInstance();
-    final data = prefs.getString('eventos');
-    if (data != null) {
-      final decoded = json.decode(data) as Map<String, dynamic>;
-      setState(() {
-        _eventos.clear();
-        decoded.forEach((key, value) {
-          final date = DateTime.parse(key);
-          final eventosList =
-              (value as List).map((e) => Evento.fromJson(e)).toList();
-          _eventos[date] = eventosList;
-        });
-      });
-    }
-  }
-
-  void _agregarEvento(DateTime fecha, Evento evento) {
-    final normalizedDate = DateTime(fecha.year, fecha.month, fecha.day);
-    setState(() {
-      if (_eventos[normalizedDate] != null) {
-        _eventos[normalizedDate]!.add(evento);
-      } else {
-        _eventos[normalizedDate] = [evento];
-      }
-    });
-    _guardarEventos();
-  }
-
-  void _mostrarDialogoAgregarEvento() {
-    String titulo = '';
-    String tipo = 'Clase';
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("Nueva asignación"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: "Título"),
-                onChanged: (value) => titulo = value,
-              ),
-              DropdownButton<String>(
-                value: tipo,
-                onChanged: (value) {
-                  if (value != null) setState(() => tipo = value);
-                },
-                items:
-                    ["Clase", "Tarea", "Examen"]
-                        .map((e) => DropdownMenuItem(value: e, child: Text(e)))
-                        .toList(),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                if (titulo.isNotEmpty && _selectedDay != null) {
-                  _agregarEvento(_selectedDay!, Evento(titulo, tipo));
-                }
-                Navigator.pop(context);
-              },
-              child: Text("Guardar"),
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text("Cancelar"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _mostrarDialogoEliminarEvento(int index) {
-    showDialog(
-      context: context,
-      builder:
-          (context) => AlertDialog(
-            title: Text("¿Eliminar evento?"),
-            content: Text("¿Estás seguro de eliminar esta asignación?"),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    final normalizedDate = DateTime(
-                      _selectedDay!.year,
-                      _selectedDay!.month,
-                      _selectedDay!.day,
-                    );
-                    final eventosDelDia = _eventos[normalizedDate]!;
-                    eventosDelDia.removeAt(index);
-                    if (eventosDelDia.isEmpty) {
-                      _eventos.remove(normalizedDate);
-                    }
-                  });
-                  _guardarEventos();
-                  Navigator.pop(context);
-                },
-                child: Text("Eliminar", style: TextStyle(color: Colors.red)),
-              ),
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: Text("Cancelar"),
-              ),
-            ],
-          ),
-    );
-  }
+class _ClasesAprobadasScreenState extends State<ClasesAprobadasScreen>{
+  List<Map<String, dynamic>> clasesAprobadasDetalles = [];
+  Map<String, dynamic>? carreraSeleccionada;
+  double porcentajeAprobado = 0.0;
 
   @override
   void initState() {
     super.initState();
-    _selectedDay = _focusedDay;
-    _cargarEventos();
+    _cargarDatos();
+  }
+
+  Future<void> _cargarDatos() async {
+  
+
+    // Cargar carrera seleccionada
+    String? carreraString = await UserPrefs.getString('carreraSeleccionada');
+    if (carreraString != null) {
+      carreraSeleccionada = json.decode(carreraString);
+    }
+
+    // Cargar materias aprobadas
+    final materiasAprobadas = await UserPrefs.getStringList('materiasSeleccionadas') ?? [];
+
+    _cargarClasesAprobadas(materiasAprobadas);
+  }
+
+  void _cargarClasesAprobadas(List<String> materiasAprobadas) {
+    List<Map<String, dynamic>> materiasAprobadasDetalles = [];
+    int totalMaterias = 0;
+
+    if (carreraSeleccionada != null) {
+      final materiasCarrera = carreraSeleccionada!['materias'] as List<dynamic>;
+      totalMaterias = materiasCarrera.length;
+
+      for (var materia in materiasCarrera) {
+        if (materiasAprobadas.contains(materia['codigo'])) {
+          materiasAprobadasDetalles.add(Map<String, dynamic>.from(materia));
+        }
+      }
+    } else {
+      Set<String> materiasAgregadas = {};
+      for (var carrera in CarrerasData.carreras) {
+        for (var materia in carrera['materias']) {
+          if (!materiasAgregadas.contains(materia['codigo'])) {
+            totalMaterias++;
+            materiasAgregadas.add(materia['codigo']);
+          }
+        }
+      }
+
+      materiasAgregadas.clear();
+
+      for (var carrera in CarrerasData.carreras) {
+        for (var materia in carrera['materias']) {
+          if (materiasAprobadas.contains(materia['codigo']) &&
+              !materiasAgregadas.contains(materia['codigo'])) {
+            materiasAprobadasDetalles.add(materia);
+            materiasAgregadas.add(materia['codigo']);
+          }
+        }
+      }
+    }
+
+    setState(() {
+      clasesAprobadasDetalles = materiasAprobadasDetalles;
+      porcentajeAprobado = totalMaterias > 0
+          ? materiasAprobadasDetalles.length / totalMaterias
+          : 0.0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Calendario Académico")),
-      body: Column(
-        children: [
-          TableCalendar<Evento>(
-            firstDay: DateTime.utc(2020, 1, 1),
-            lastDay: DateTime.utc(2070, 12, 31),
-            focusedDay: _focusedDay,
-            calendarFormat: _calendarFormat,
-            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-            eventLoader: _getEventosParaDia,
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-              });
-            },
-            onFormatChanged: (format) {
-              setState(() => _calendarFormat = format);
-            },
-            onPageChanged: (focusedDay) {
-              _focusedDay = focusedDay;
-            },
-            calendarStyle: CalendarStyle(
-              markerDecoration: BoxDecoration(
-                color: Colors.blue,
-                shape: BoxShape.circle,
+      appBar: AppBar(
+        title: const Text('Clases Aprobadas', style: TextStyle(color: Colors.white)),
+        backgroundColor: const Color(0xFF1D9FCB),
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20,),
+            Center(
+              child: const Text(
+                'TU PROGRESO',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Expanded(
-            child: ListView(
-              children:
-                  _getEventosParaDia(_selectedDay!).asMap().entries.map((
-                    entry,
-                  ) {
-                    final index = entry.key;
-                    final evento = entry.value;
-                    return ListTile(
-                      title: Text(evento.titulo),
-                      subtitle: Text(evento.tipo),
-                      leading: Icon(
-                        evento.tipo == "Examen"
-                            ? Icons.assignment
-                            : evento.tipo == "Tarea"
-                            ? Icons.task
-                            : Icons.school,
-                      ),
-                      onLongPress: () => _mostrarDialogoEliminarEvento(index),
-                    );
-                  }).toList(),
+            const SizedBox(height: 15),
+            LinearPercentIndicator(
+              lineHeight: 25,
+              percent: porcentajeAprobado.clamp(0, 1),
+              center: Text(
+                '${(porcentajeAprobado * 100).toInt()}%',
+                style: const TextStyle(color: Color(0xFF1D9FCB)),
+              ),
+              barRadius: const Radius.circular(12),
+              backgroundColor: Colors.grey.shade200,
+              progressColor: Color(0xFF1D9FCB),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: SizedBox(
-        width: 150,
-        height: 56,
-        child: FloatingActionButton.extended(
-          onPressed: _mostrarDialogoAgregarEvento,
-          backgroundColor: Colors.blue,
-          label: Text(
-            'Añadir evento',
-            style: TextStyle(fontSize: 16, color: Colors.white),
-          ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: clasesAprobadasDetalles.isEmpty
+                  ? const Center(
+                      child: Text(
+                        "No hay clases aprobadas aún.",
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    )
+                  : ListView.builder(
+                      itemCount: clasesAprobadasDetalles.length,
+                      itemBuilder: (context, index) {
+                        final materia = clasesAprobadasDetalles[index];
+                        return Card(
+                          elevation: 4,
+                          margin: const EdgeInsets.symmetric(vertical: 8),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: IntrinsicHeight(
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 8,
+                                  decoration: BoxDecoration(
+                                    color: Colors.primaries[index % Colors.primaries.length],
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(12),
+                                      bottomLeft: Radius.circular(12),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: ListTile(
+                                    contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 16, vertical: 12),
+                                    title: Text(
+                                      materia['asignatura'] ?? '',
+                                      style: const TextStyle(fontWeight: FontWeight.bold),
+                                    ),
+                                    subtitle: Text(
+                                      'Código: ${materia['codigo']} | UV: ${materia['uv']} | Requisito: ${materia['requisito']}',
+                                      style: const TextStyle(color: Colors.grey, fontSize: 12),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+            ),
+          ],
         ),
       ),
     );
